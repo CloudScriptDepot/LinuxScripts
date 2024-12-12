77ad98eae141a84bb4d154e29bfcf4743c96ad87
CloudScriptDepot <jflores2k@outlook.com>
2024-12-12 11:48:59 -0500

    Trellix_Uninstaller_Download.sh

--- Trellix_Uninstaller_Download.sh
+++ Trellix_Uninstaller_Download.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
-for i in $(cat serverlist.txt)
-do
-    echo "====================================================================================" >> output.txt
-    sshpass -p "" ssh -tt -o StrictHostKeyChecking=no $i "
-        sudo su - <<EOF
-        hostname
-        curl -LO https://raw.githubusercontent.com/CloudScriptDepot/LinuxScripts/refs/heads/main/Trellix_Uninstaller_Download.sh
-        chmod +x Trellix_Uninstaller_Download.sh
-        ./Trellix_Uninstall_Download.sh
-        systemctl status xagt
-EOF
-    " >> output.txt
-    echo "====================================================================================" >> output.txt
-done
 
+# Check if the system uses RPM or DEB package management
+if command -v rpm &> /dev/null; then
+    package_manager="rpm"
+elif command -v dpkg &> /dev/null; then
+    package_manager="dpkg"
+else
+    echo "Neither RPM nor DEB package management system found."
+    exit 1
+fi
+
+# Function to remove RPM package
+remove_rpm() {
+    rpm_output=$(rpm -qa | grep xagt)
+    if [ ! -z "$rpm_output" ]; then
+        echo "RPM package found: $rpm_output"
+        rpm -e "$rpm_output"
+    else
+        echo "xagt RPM package not found."
+    fi
+}
+
+# Function to remove DEB package
+remove_deb() {
+    deb_output=$(dpkg -l | grep xagt | awk '{print $2}')
+    if [ ! -z "$deb_output" ]; then
+        echo "DEB package found: $deb_output"
+        dpkg --purge "$deb_output"
+    else
+        echo "xagt DEB package not found."
+    fi
+}
+
+# Remove the package based on the detected package manager
+if [ "$package_manager" == "rpm" ]; then
+    remove_rpm
+elif [ "$package_manager" == "dpkg" ]; then
+    remove_deb
+fi
